29f2bdac811cc529aa5749d9e46d1c36a00c7b39
Saimon <felipesaimon.lima@gmail.com>
2022-01-25 09:27:24 -0300

    up

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function PaginaInicial() {
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
             backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            backgroundImage: 'url(https://observatoriodocinema.uol.com.br/wp-content/uploads/2020/04/Alvin-e-os-Esquilos.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
