d07a33458414ac3ce1cb1871298a516e6dfbf7d0
Saimon Lima <felipesaimon.lima@gmail.com>
2022-01-26 14:30:10 -0300

    dia 2

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,6 @@ import appConfig from  '../config.json';
 import { useRouter } from 'next/router'
 
 //isso é um componente react - uma função que retorna um trecho html
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 
 //Props nesse caso retorna um objeto então é preciso tratar o filho
@@ -79,7 +51,6 @@ export default function PaginaInicial() {
     console.log('UseState', username)
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
